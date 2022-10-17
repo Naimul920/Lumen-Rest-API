6b5e3acd33194dec9ba8f9a2fa2b17cc02817e36
Naimul Islam <naimulislam920@gmail.com>
2022-10-18 02:00:36 +0600

    Start

---
+++ blog/app/Http/Controllers/MyController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+class MyController extends Controller
+{
+    function Catch(Request $request){
+        return $request->header("name");
+    }
+
+}
